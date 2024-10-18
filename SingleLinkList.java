afb12d89451d07e3bb417df65fb9cc1ae6af9a1f
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-18 23:00:43 +0530

    Add files via upload

--- SingleLinkList.java
+++ SingleLinkList.java
@@ -14,18 +14,29 @@ public class SingleLinkList {
         }
     }
 
+    public void Display() {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.data + "--> ");
+            current = current.next;
+        }
+        System.out.println("No data");
+    }
+
     public static void main(String[] args) {
         SingleLinkList sll = new SingleLinkList();
         sll.head = new ListNode(10);
-        ListNode second = new ListNode(1);
-        ListNode third = new ListNode(8);
-        ListNode forth = new ListNode(11);
+        ListNode second = new ListNode(5);
+        ListNode third = new ListNode(4);
+        ListNode forth = new ListNode(9);
 
         // Now we will connect them togather in a form of chain
 
         sll.head.next = second;
         second.next = third;
         third.next = forth;
+
+        sll.Display();
     }
 
 }

4adb3a48a31aa99130d12b7b93f285fb50843d96
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-12-16 09:51:18 +0530

    Add files via upload

---
+++ FloatConversion.java
@@ -0,0 +1,18 @@
+
+public class FloatConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	        float Float = 13;
+
+	        float autoboxedInteger = Float; 
+	        System.out.println("Autoboxed Integer: " + autoboxedInteger);
+
+	        float constructedInteger = new Float(Float); 
+	        System.out.println("Constructed Integer: " + constructedInteger);
+
+	}
+
+}
